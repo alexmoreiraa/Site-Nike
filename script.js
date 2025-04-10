a76ad448046f3383ff11fcd7136b0bcda6f992c4
alexmoreiraa <alexjuniorsilvamoreira@gmail.com>
2025-04-09 22:11:39 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let body = document.querySelector("body");
+let tenis= document.querySelector(".imagem-tenis")
+
+
+
+function mudarVisual(cor,imagem) {
+    body.style.backgroundColor = cor;
+    tenis.src =imagem;
+}
